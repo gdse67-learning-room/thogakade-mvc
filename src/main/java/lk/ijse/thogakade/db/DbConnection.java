a1cfdfb65c0512e72ca4ed83b54a930e148cdefe
danu <danujagreru@gmail.com>
2023-10-23 10:11:34 +0530

    creat DbConnection.java

---
+++ src/main/java/lk/ijse/thogakade/db/DbConnection.java
@@ -0,0 +1,31 @@
+package lk.ijse.thogakade.db;
+
+/*
+    @author DanujaV
+    @created 10/23/23 - 10:08 AM   
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static DbConnection dbConnection;
+    private Connection connection;
+
+    private DbConnection() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kade",
+                "root",
+                "Danu25412541@"
+        );
+    }
+
+    public static DbConnection getInstance() throws SQLException {
+        return (null == dbConnection) ? dbConnection = new DbConnecticon() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
